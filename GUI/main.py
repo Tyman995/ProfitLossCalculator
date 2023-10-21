1c5ebab52d6063c2c504228454cd74454256e8b7
Tyman995 <TheTyman9995@gmail.com>
2023-10-21 19:43:06 -0400

    Added a basic Database file that creates a test db, will be altered later

--- GUI/main.py
+++ GUI/main.py
@@ -1,22 +1,22 @@
 import customtkinter as ctk
 import tkinter as tk
 import sqlite3
-import database
 
 root = ctk.CTk()
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('green')
 
 font1 = ('Times New Roman', 16)
+font2 = ('Times New Roman', 12)
 
-root.geometry("1920x1080")
+root.geometry("500x500")
 
 root.title("Profit Loss Calculator")
 
 testTextBox = ctk.CTkTextbox(root, font=font1)
 testTextBox.pack()
 
-button = ctk.CTkButton(root, text="IM A BUTTON CLICK ME!", font = ('Times New Roman', 12))
+button = ctk.CTkButton(root, text="IM A BUTTON CLICK ME!", font = font2)
 button.pack(padx=15,pady=15)
 
 root.mainloop()

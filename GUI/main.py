f677e1e1d52a7be76ae4bbab1e1f598773dff607
Tyman995 <TheTyman9995@gmail.com>
2023-11-06 16:57:19 -0500

    cleaned up reset button by also clearing field entries

--- GUI/main.py
+++ GUI/main.py
@@ -29,11 +29,8 @@ root.config(menu=my_menu)
 
 
 #commands
-def a_command():
-    pass
-
-def file_new():
-    pass
+#def file_new():
+#    pass
 
 def file_save():
     db.save_as_csv()
@@ -73,12 +70,13 @@ def lookup_transactions():
 def reset_tree():
     add_db_to_tree()
     remove_labels()
+    clear_field()
     results_display()
     
 #create menu items
 file_menu = Menu(my_menu, tearoff=0)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New File", command=file_new)
+#file_menu.add_command(label="New File", command=file_new)
 file_menu.add_command(label="Save File", command=file_save)
 file_menu.add_command(label="Exit", command=root.quit)
 
